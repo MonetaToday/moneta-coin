32dab1f87a65601801ac3470153bb0bdc3b061f0
Moneta Today <develop@moneta.today>
2021-10-01 23:15:53 +0700

    Update denom of main coin

--- custom_scripts/init.sh
+++ custom_scripts/init.sh
@@ -2,7 +2,7 @@ mkdir -p $DAEMON_HOME/cosmovisor/genesis/bin
 cp $BC_SRC/build/simd $DAEMON_HOME/cosmovisor/genesis/bin
 
 $BC_SRC/build/simd init $NODE_NAME --chain-id $CHAIN_NAME --home $DAEMON_HOME
-$BC_SRC/build/simd keys --home $DAEMON_HOME add $VALIDATOR_NAME > $BC_SRC/custom_scripts/$VALIDATOR_NAME.keys
+$BC_SRC/build/simd keys --home $DAEMON_HOME add $VALIDATOR_NAME > $BC_SRC/private/$VALIDATOR_NAME.keys
 $BC_SRC/build/simd add-genesis-account --home $DAEMON_HOME $($BC_SRC/build/simd keys --home $DAEMON_HOME show $VALIDATOR_NAME -a) 10000000000000000micromoneta
 $BC_SRC/build/simd gentx --home $DAEMON_HOME --chain-id $CHAIN_NAME $VALIDATOR_NAME 100000000micromoneta
 $BC_SRC/build/simd collect-gentxs --home $DAEMON_HOME
