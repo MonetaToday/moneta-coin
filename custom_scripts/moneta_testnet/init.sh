a2f6f09b282c3b2f9a1e12890947051db2681281
Moneta Today <develop@moneta.today>
2021-10-18 00:11:34 +0700

    Add testnet scripts

--- custom_scripts/moneta_testnet/init.sh
+++ custom_scripts/moneta_testnet/init.sh
@@ -1,7 +1,7 @@
 export BC_SRC=$1
 
 export DAEMON_NAME=monetad
-export DAEMON_HOME=$HOME/.moneta_testnet
+export DAEMON_HOME=$HOME/.moneta
 
 export CHAIN_NAME=moneta_testnet
 export ACCOUNT_NAME=$2
@@ -11,8 +11,8 @@ mkdir -p $BC_SRC/private
 mkdir -p $DAEMON_HOME/cosmovisor/genesis/bin
 cp $BC_SRC/build/$DAEMON_NAME $DAEMON_HOME/cosmovisor/genesis/bin
 
-$BC_SRC/build/$DAEMON_NAME --home $DAEMON_HOME init $NODE_NAME --chain-id $CHAIN_NAME
+$BC_SRC/build/$DAEMON_NAME init $NODE_NAME --chain-id $CHAIN_NAME
 $BC_SRC/build/$DAEMON_NAME keys add $ACCOUNT_NAME > $BC_SRC/private/$ACCOUNT_NAME.keys
-$BC_SRC/build/$DAEMON_NAME --home $DAEMON_HOME tendermint show-node-id > $BC_SRC/private/$ACCOUNT_NAME.node
-$BC_SRC/build/$DAEMON_NAME --home $DAEMON_HOME add-genesis-account $($BC_SRC/build/$DAEMON_NAME keys show $ACCOUNT_NAME -a  --home $DAEMON_HOME) $4
-$BC_SRC/build/$DAEMON_NAME --home $DAEMON_HOME gentx --chain-id $CHAIN_NAME $ACCOUNT_NAME $5
+$BC_SRC/build/$DAEMON_NAME tendermint show-node-id > $BC_SRC/private/$ACCOUNT_NAME.node
+$BC_SRC/build/$DAEMON_NAME add-genesis-account $($BC_SRC/build/$DAEMON_NAME keys show $ACCOUNT_NAME -a) $4
+$BC_SRC/build/$DAEMON_NAME gentx --chain-id $CHAIN_NAME $ACCOUNT_NAME $5
