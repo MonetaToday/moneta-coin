a9852ac0a365bc931137757f1892304bef4d10bd
Moneta Today <develop@moneta.today>
2021-10-18 00:18:48 +0700

    Update node testnet ids

--- custom_scripts/moneta_testnet/copy_configs.sh
+++ custom_scripts/moneta_testnet/copy_configs.sh
@@ -3,5 +3,5 @@ export BC_SRC=$1
 export DAEMON_NAME=monetad
 export DAEMON_HOME=$HOME/.moneta
 
-cp $BC_SRC/initial_configs/config.toml $DAEMON_HOME/config/config.toml
-cp $BC_SRC/initial_configs/app.toml $DAEMON_HOME/config/app.toml
+cp $BC_SRC/initial_configs/testnet-config.toml $DAEMON_HOME/config/config.toml
+cp $BC_SRC/initial_configs/testnet-genesis.json $DAEMON_HOME/config/genesis.json
