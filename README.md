5f2f4ee469c002dcbaf3a58e227216f0c4326f06
Moneta Today <develop@moneta.today>
2021-10-17 22:59:17 +0700

    Add initial script

--- README.md
+++ README.md
@@ -3,3 +3,17 @@ This network is managed by community voting.
 The coins which are deposited to a proposal will burn either:
 1) MinDeposit has't been reached by the end of deposit period;
 2) VetoThreshold votes count is greater than 0.334% of voting power.
+
+
+
+Firstly install Go:
+  https://golang.org/doc/install
+
+Secondly clone the repository:
+  git clone https://github.com/MonetaToday/moneta-coin.git -b monetacoin-develop
+
+In Ubuntu please install:
+  apt install make gcc
+
+And make the build:
+  make build; make cosmovisor
---
+++ custom_scripts/moneta_init.sh
@@ -0,0 +1,23 @@
+export BC_SRC=$1
+
+export DAEMON_NAME=monetad
+export DAEMON_HOME=$HOME/.moneta
+
+export CHAIN_NAME=moneta
+export ACCOUNT_NAME=$2
+export NODE_NAME=$3
+
+rm -R $DAEMON_HOME
+
+mkdir -p $DAEMON_HOME/cosmovisor/genesis/bin
+cp $BC_SRC/build/$DAEMON_NAME $DAEMON_HOME/cosmovisor/genesis/bin
+
+$BC_SRC/build/$DAEMON_NAME init $NODE_NAME --chain-id $CHAIN_NAME
+$BC_SRC/build/$DAEMON_NAME keys add $ACCOUNT_NAME > $BC_SRC/private/$ACCOUNT_NAME.keys
+$BC_SRC/build/$DAEMON_NAME tendermint show-node-id > $BC_SRC/private/$ACCOUNT_NAME.node
+$BC_SRC/build/$DAEMON_NAME add-genesis-account $($BC_SRC/build/$DAEMON_NAME keys show $ACCOUNT_NAME -a) $4
+$BC_SRC/build/$DAEMON_NAME gentx --chain-id $CHAIN_NAME $ACCOUNT_NAME $5
+$BC_SRC/build/$DAEMON_NAME collect-gentxs
+
+# cp $BC_SRC/initial_configs/config.toml $DAEMON_HOME/config/config.toml
+# cp $BC_SRC/initial_configs/app.toml $DAEMON_HOME/config/app.toml
