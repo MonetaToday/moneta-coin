1c3b417e25b71c3094d5fe13120ebf7679d9e5f8
Moneta Today <develop@moneta.today>
2021-10-18 00:51:50 +0700

    Update testnet initial configs

--- custom_scripts/moneta_testnet/init.sh
+++ custom_scripts/moneta_testnet/init.sh
@@ -14,5 +14,3 @@ cp $BC_SRC/build/$DAEMON_NAME $DAEMON_HOME/cosmovisor/genesis/bin
 $BC_SRC/build/$DAEMON_NAME init $NODE_NAME --chain-id $CHAIN_NAME
 $BC_SRC/build/$DAEMON_NAME keys add $ACCOUNT_NAME > $BC_SRC/private/$ACCOUNT_NAME.keys
 $BC_SRC/build/$DAEMON_NAME tendermint show-node-id > $BC_SRC/private/$ACCOUNT_NAME.node
-$BC_SRC/build/$DAEMON_NAME add-genesis-account $($BC_SRC/build/$DAEMON_NAME keys show $ACCOUNT_NAME -a) $4
-$BC_SRC/build/$DAEMON_NAME gentx --chain-id $CHAIN_NAME $ACCOUNT_NAME $5
