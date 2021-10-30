bbade6422a81e347500c68025a959931640f3ddd
Moneta Today <develop@moneta.today>
2021-10-30 12:44:50 +0700

    Add update script

--- custom_scripts/moneta_testnet/update.sh
+++ custom_scripts/moneta_testnet/update.sh
@@ -7,5 +7,10 @@ git pull
 make build
 
 systemctl stop monetad.service
+echo "Service monetad.service restarted"
+
 cp $BC_SRC/build/$DAEMON_NAME $DAEMON_HOME/cosmovisor/genesis/bin
+echo "Build bin copied"
+
 systemctl start monetad.service
+echo "Service monetad.service started"
