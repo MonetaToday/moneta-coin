9a74e76330ed329fea6b152c3373fb94c69067df
Moneta Today <develop@moneta.today>
2021-12-15 19:38:34 +0300

    Add mainnet init config and genesis

--- custom_scripts/moneta_mainnet/copy_configs.sh
+++ custom_scripts/moneta_mainnet/copy_configs.sh
@@ -4,4 +4,4 @@ export DAEMON_NAME=monetad
 export DAEMON_HOME=$HOME/.moneta
 
 cp $BC_SRC/initial_configs/mainnet-config.toml $DAEMON_HOME/config/config.toml
-cp $BC_SRC/initial_configs/meinnet-genesis.json $DAEMON_HOME/config/genesis.json
+cp $BC_SRC/initial_configs/mainnet-genesis.json $DAEMON_HOME/config/genesis.json
